3debe53e6b548c1229c28f1a29429ea297f9cf30
dengqing <dengqing0821@gmail.com>
2022-04-26 21:34:52 +0800

    refactor: improve Link implementation

--- packages/theme/src/components/Link.tsx
+++ packages/theme/src/components/Link.tsx
@@ -1,64 +1,58 @@
+import type { ForwardRefRenderFunction } from 'react'
 import React from 'react'
-import type { LinkProps, NavLinkProps } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import type { LinkProps } from 'react-router-dom'
+import { Link as RouteLink } from 'react-router-dom'
+
+const externalIcon = <svg
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+  x="0px"
+  y="0px"
+  viewBox="0 0 100 100"
+  width="15"
+  height="15"
+  className="__dumi-default-external-link-icon"
+>
+  <path
+    fill="currentColor"
+    d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
+  />
+  <polygon
+    fill="currentColor"
+    points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
+  />
+</svg>
 
 /**
  * Link component wrapper for render external link
  * @param Component   original Link component
  */
-export const LinkWrapper = (Component: React.FC<LinkProps | NavLinkProps>) => {
-  return function LinkCustom({ to, ...props }: LinkProps | NavLinkProps) {
-    const isExternal = /^(\w+:)?\/\/|^(mailto|tel):/.test(to as string) || !to
-    const hasComplexChildren = React.isValidElement(props.children)
+const Link: ForwardRefRenderFunction<HTMLAnchorElement, LinkProps> = ({ to, onClick, ...props }, ref) => {
+  const isExternal = /^(\w+:)?\/\/|^(mailto|tel):/.test(to as string) || !to
+  const hasComplexChildren = React.isValidElement(props.children)
 
-    return (
-      <Component
-        to={(to as any) || ''}
-        component={
-          isExternal
-            ? () => (
-                <a target="_blank" rel="noopener noreferrer" href={to as string}>
-                  {props.children}
-                  {to && !hasComplexChildren && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 100 100"
-                      width="15"
-                      height="15"
-                      className="__dumi-default-external-link-icon"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-                      />
-                      <polygon
-                        fill="currentColor"
-                        points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-                      />
-                    </svg>
-                  )}
-                </a>
-              )
-            : undefined
+  const externalProps = isExternal ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+
+  return (
+    <RouteLink
+      ref={ref}
+      to={(to as any) || ''}
+      {...externalProps}
+      onClick={(...args) => {
+        if (!isExternal) {
+          window.scrollTo({
+            top: 0,
+          })
         }
-        {...props}
-        {...(isExternal
-          ? {}
-          : {
-              // scroll to top while change url
-              onClick(...args) {
-                window.scrollTo({
-                  top: 0,
-                })
-                props.onClick?.apply(this, args)
-              },
-            })}
-      />
-    )
-  }
+        onClick?.(...args)
+      }}
+    >
+      {props.children}
+      {isExternal && to && !hasComplexChildren && (
+        externalIcon
+      )}
+    </RouteLink>
+  )
 }
 
-export default LinkWrapper(Link)
+export default React.forwardRef(Link)
--- packages/theme/src/components/NavLink.tsx
+++ packages/theme/src/components/NavLink.tsx
@@ -1,4 +1,92 @@
-import { NavLink } from 'react-router-dom'
-import { LinkWrapper } from './Link'
+import React from 'react'
+import type { LinkProps } from 'react-router-dom'
+import { useLocation, useResolvedPath } from 'react-router-dom'
+import Link from './Link'
 
-export default LinkWrapper(NavLink)
+export interface NavLinkProps
+  extends Omit<LinkProps, 'className' | 'style' | 'children'> {
+  children?:
+  | React.ReactNode
+  | ((props: { isActive: boolean }) => React.ReactNode)
+  caseSensitive?: boolean
+  className?: string | ((props: { isActive: boolean }) => string | undefined)
+  end?: boolean
+  active?: boolean
+  style?:
+  | React.CSSProperties
+  | ((props: { isActive: boolean }) => React.CSSProperties)
+}
+
+/**
+ * A <Link> wrapper that knows if it's "active" or not.
+ */
+// eslint-disable-next-line react/display-name
+const NavLink = React.forwardRef<HTMLAnchorElement, NavLinkProps>(
+  (
+    {
+      'aria-current': ariaCurrentProp = 'page',
+      caseSensitive = false,
+      className: classNameProp = '',
+      end = false,
+      style: styleProp,
+      to,
+      children,
+      active,
+      ...rest
+    },
+    ref,
+  ) => {
+    const location = useLocation()
+    const path = useResolvedPath(to)
+
+    let locationPathname = location.pathname
+    let toPathname = path.pathname
+    if (!caseSensitive) {
+      locationPathname = locationPathname.toLowerCase()
+      toPathname = toPathname.toLowerCase()
+    }
+
+    const isActive
+      = active !== undefined
+        ? active
+        : (locationPathname === toPathname
+          || (!end
+            && locationPathname.startsWith(toPathname)
+            && locationPathname.charAt(toPathname.length) === '/'))
+
+    const ariaCurrent = isActive ? ariaCurrentProp : undefined
+
+    let className: string | undefined
+    if (typeof classNameProp === 'function') {
+      className = classNameProp({ isActive })
+    }
+    else {
+      // If the className prop is not a function, we use a default `active`
+      // class for <NavLink />s that are active. In v5 `active` was the default
+      // value for `activeClassName`, but we are removing that API and can still
+      // use the old default behavior for a cleaner upgrade path and keep the
+      // simple styling rules working as they currently do.
+      className = [classNameProp, isActive ? 'active' : null]
+        .filter(Boolean)
+        .join(' ')
+    }
+
+    const style
+      = typeof styleProp === 'function' ? styleProp({ isActive }) : styleProp
+
+    return (
+      <Link
+        {...rest}
+        aria-current={ariaCurrent}
+        className={className}
+        ref={ref}
+        style={style}
+        to={to}
+      >
+        {typeof children === 'function' ? children({ isActive }) : children}
+      </Link>
+    )
+  },
+)
+
+export default NavLink
