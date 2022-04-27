67d89fb1dbe2323a1543744f5540a27ee5a17c8d
dengqing <1247748612@qq.com>
2022-04-27 13:41:16 +0800

    feat: display nav ready

--- packages/plugin-dumi/src/node/routes/routes.ts
+++ packages/plugin-dumi/src/node/routes/routes.ts
@@ -1,6 +1,4 @@
 import type { ResolvedConfig } from 'vite'
-
-import type { RouteObject } from 'react-router-dom'
 import type { ResolveFunction } from '../transformer'
 import { transform } from '../transformer'
 import { detectMarkdowns } from './markdown'
@@ -20,6 +18,7 @@ interface MetaData {
   slugs: any[]
 }
 
+
 export const loadMarkdowns = async(root: string, resolve: ResolveFunction) => {
   const paths = detectMarkdowns(root)
 
@@ -37,83 +36,131 @@ export const loadMarkdowns = async(root: string, resolve: ResolveFunction) => {
   return transformResults
 }
 
+type PickPromiseType<T> = T extends Promise<infer U> ? U : T
+type SourcesType = PickPromiseType<ReturnType<typeof loadMarkdowns>>
 
 
+export const generateRoutes = (Sources: SourcesType) => {
 
-export const generateRoutes = async(config: ResolvedConfig, resolve: ResolveFunction) => {
+  return `
+      const buildingRoutes = () => {
+        const __runtimeComponent__ = (src) => {
+          switch (src) {
+            ${
+              Sources.map((s) => {
+                return `case ${JSON.stringify(s.id)}: return lazy(() => import(${JSON.stringify(s.path)}));`
+              }).join('\n')
+            }
+          }
+        }
+
+      const indexRE = /index$/
+      const routes: RouteObject[] = [];
+
+        Sources.forEach((item) => {
+          const { paths } = parsedPath(item)
+          let curRoutes = routes
+          paths.forEach((p, index) => {
+            let cr = curRoutes.find(r => r.path === (p || '/'))
+            const children: RouteObject[] = cr?.children || []
+            const isIndex = indexRE.test(p)
+
+            if (!cr) {
+              cr = {
+                path: isIndex ? undefined : p || '/',
+                children,
+                index: isIndex,
+              }
+              curRoutes.push(cr)
+            }
+            if (index === paths.length - 1) {
+              let Component = __runtimeComponent__(item.id)
+              Object.assign(cr, {
+                path: isIndex ? paths.slice(0, -1).join('/') || '/' : paths.join('/'),
+                element: <Component />,
+                index: isIndex,
+                title: item.meta?.title,
+                meta: item.meta
+              })
+            }
+            curRoutes = children
+          })
+        })
+
+      return routes
+    }
+
+    export const routes = buildingRoutes()
+  `
+}
+
+export const generateNav = () => {
+
+  return `
+    function generateNav() {
+      const localesNav = {}
+      Sources.filter(s => s.meta.nav).forEach((source) => {
+        const { paths, locale } = parsedPath(source)
+        const navList = (localesNav[locale] || ( localesNav[locale] = []));
+        const nav = {
+          ...source.meta?.nav,
+          path: paths.slice(0, -1).join('/'),
+        }
+        navList.push(nav)
+      })
+      Object.keys(localesNav).forEach((locale) => {
+        localesNav[locale] = localesNav[locale].sort((prev, next) => {
+          const prevOrder = typeof prev.order === 'number' ? prev.order : Infinity;
+          const nextOrder = typeof next.order === 'number' ? next.order : Infinity;
+          // compare order meta config first
+          const metaOrder = prevOrder === nextOrder ? 0 : prevOrder - nextOrder;
+          // last compare path length
+          const pathOrder = prev.path.length - next.path.length;
+          // then compare title ASCII
+          const ascOrder = prev.title > next.title ? 1 : prev.title < next.title ? -1 : 0;
+          return metaOrder || pathOrder || ascOrder;
+        })
+      })
+      return localesNav
+    }
+
+    export const nav = generateNav()
+  `  
+}
+
+
+export const dumiProvider = async(config: ResolvedConfig, resolve: ResolveFunction) => {
   const Sources = await loadMarkdowns(config.root, resolve)
   const locales = [['en-US', 'English'], ['zh-CN', '中文']].map(i => i[0])
-
-
+  
   const code = `
     import React, { lazy } from 'react'
 
-    const __runtimeComponent__ = (src) => {
-      switch (src) {
-        ${
-          Sources.map((s) => {
-            return `case ${JSON.stringify(s.id)}: return lazy(() => import(${JSON.stringify(s.path)}));`
-          }).join('\n')
-        }
-      }
-    }
-
     const Sources = ${JSON.stringify(Sources)}
+    const locales = ${JSON.stringify(locales)}
 
-    const firstLocale = ${JSON.stringify(locales[0])}
+    export const parsedPath = (item: any) => {
+    const firstLocale = locales[0]
     const localesRE = new RegExp('\\\\.(${locales.join('|')})$')
-    const indexRE = /index$/
+    const routePathRE = new RegExp("(docs|src)([\\\\/\\\\w\\\\d\\\\-_\\\\.]+)?.md");
 
-    const buildingRoutes = () => {
-      const routePathRE = new RegExp("(docs|src)([\\\\/\\\\w\\\\d\\\\-_\\\\.]+)?.md");
-      const routes: RouteObject[] = [];
+    const routePath = routePathRE.exec(item.id)![2]
+    let paths = routePath.split('/')
+    const namePath = paths[paths.length - 1]
+    paths = paths.slice(0, -1)
+    const metaPath = (item.meta.nav?.path || '') + (item.meta.group?.path || '')
+    paths = metaPath ? paths.splice(0, paths.length - 2, ...metaPath.split('/')) : paths
 
-      Sources.forEach((item) => {
-        const routePath = routePathRE.exec(item.id)![2]
-        let paths = routePath.split('/')
-        const namePath = paths[paths.length - 1]
-        paths = paths.slice(0, -1)
-        const metaPath = (item.meta.nav?.path || '') + (item.meta.group?.path || '')
-        paths = metaPath ? paths.splice(0, paths.length - 2, ...metaPath.split('/')) : paths
+    const locale = localesRE.exec(namePath)?.[1]
+    if (locale && locale !== firstLocale) {
+      paths.splice(0, 1, '/' + locale)
+    }
+    paths.push(namePath.replace(localesRE, ''))
 
-        const locale = localesRE.exec(namePath)?.[1]
-        if (locale && locale !== firstLocale) {
-          paths.splice(0, 1, '/' + locale)
-        }
-        paths.push(namePath.replace(localesRE, ''))
-
-        let curRoutes = routes
-        paths.forEach((p, index) => {
-          let cr = curRoutes.find(r => r.path === (p || '/'))
-          const children: RouteObject[] = cr?.children || []
-          const isIndex = indexRE.test(p)
-
-          if (!cr) {
-            cr = {
-              path: isIndex ? undefined : p || '/',
-              children,
-              index: isIndex,
-            }
-            curRoutes.push(cr)
-          }
-          if (index === paths.length - 1) {
-            let Component = __runtimeComponent__(item.id)
-            Object.assign(cr, {
-              path: isIndex ? paths.slice(0, -1).join('/') || '/' : paths.join('/'),
-              element: <Component />,
-              index: isIndex,
-              title: item.meta?.title,
-              meta: item.meta
-            })
-          }
-          curRoutes = children
-        })
-      })
-
-    return routes
+    return { paths, locale: locale || firstLocale }
   }
-
-  export default buildingRoutes()
+    ${await generateRoutes(Sources)}
+    ${await generateNav()}
   `
 
   return transformSync(code, {
