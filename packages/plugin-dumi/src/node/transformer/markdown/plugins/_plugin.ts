9474d9cc5dbaf4ad9a42147742e33fb164db2f16
dengqing <dengqing0821@gmail.com>
2022-05-04 22:59:49 +0800

    feat: improve plugin tmp

--- packages/plugin-dumi/src/node/transformer/markdown/plugins/_plugin.ts
+++ packages/plugin-dumi/src/node/transformer/markdown/plugins/_plugin.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from 'unified'
 import type { Element } from 'hast'
-export function wrapper(): Plugin {
-  return (node: Element) => {
+export const plugin: Plugin<[], Element> = function() {
+  return () => {
   }
 }
